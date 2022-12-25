b2dc9c687e09535d4a7dad1bf3efd2d961df66bc
neer <neer5684@gmail.com>
2022-12-25 16:35:57 +0545

    csp meta tag

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,9 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <link rel="shortcut icon" href="{{URL::asset('/movie.png')}}">
 
-    
+
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://getbootstrap.com/docs/5.2/assets/css/docs.css" rel="stylesheet">
