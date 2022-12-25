c1d92f19111acfdc723943ca99a84140d6d3be8e
neer <neer5684@gmail.com>
2022-12-25 16:29:36 +0545

    ajax setup

--- resources/views/title.blade.php
+++ resources/views/title.blade.php
@@ -612,6 +612,16 @@ $home='active';
 @section('extraScripts')
 <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 <script>
+    $.ajaxSetup({
+
+        headers: {
+
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+
+        }
+
+    });
+
     var userInfo = null;
     var favCount = parseInt("{{ $noOfFaviourite }}");
 
