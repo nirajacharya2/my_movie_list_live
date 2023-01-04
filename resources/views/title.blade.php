4b7eba16cd65a3e2e92316f839777869e84e3f19
neer <neer5684@gmail.com>
2023-01-04 18:16:46 +0545

    a

--- resources/views/title.blade.php
+++ resources/views/title.blade.php
@@ -614,6 +614,7 @@ $home='active';
 <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 <script>
     var userInfo = null;
+    var submitForm = true;
     var favCount = parseInt("{{ $noOfFaviourite }}");
 
     // console.log(userInfo);
